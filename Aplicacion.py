3fbface54de2af388852250bdc553c609d691c1e
Oren <20A3110207@untels.edu.pe>
2022-11-07 10:42:06 -0500

    Desarrolado por Arones Paz, Oren Octavio

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,7 +1,8 @@
 import sqlite3 as sql
-
+#Metodos del menu
 def mostrar_menu(opciones):
-    print('Menu Opciones:')
+
+    print('Menu opciones:')
     for clave in sorted(opciones):
         print(f'{clave}) {opciones[clave][0]}')           
 
@@ -24,28 +25,74 @@ def generar_menu(opciones,opcion_salida):
 def menu_principal():
     opciones = {
         '1':('Registrar', registrar),
-        '2':('Eliminar', eliminar),
+        '2':('Eliminar',eliminar),
         '3':('Editar', editar),
         '4':('Listar', listar),      
         '5':('Salir', salir)
     }       
 
     generar_menu(opciones,'5')
+#---------------------------------------------------------------------------------------
+
+#Metodos de opciones
+
 
 def registrar():
-    print('Registrado exitosamente')
-
+    print('opcion registrar')
 def eliminar():
-    print('Eliminado Correctamente')
-
+    print('opcion eliminar')
 def editar():
-    print('Editado correctamente')
-
+    print('opcion editar')
 def listar():
-    print('Lista de Productos')
-
+    print('opcion listar')
 def salir():
-    print('Saliendo del menu\n')
+    print('opcion salir')
 
-if __name__== '__main__':
-    menu_principal()
+#-------------------------------------------------------
+#Metodos Base de Datos
+def crearBD():
+    conn = sql.connect("Arones_Bravo_Huanuco_Nanquen_almacen.db")
+    conn.commit()
+    conn.close()
+    
+def crearTabla():
+    conn = sql.connect("Arones_Bravo_Huanuco_Nanquen_almacen.db")
+    cursor = conn.cursor()
+    cursor.execute(
+    """CREATE TABLE Producto (
+        idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+        codigo text,
+        nombre text,
+        precio float 
+    )"""
+    )
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__" :
+    #menu_principal()
+    #crearBD()
+    #crearTabla()
+    pass
+
+    
+
+
+
+
+
+
+
+
+
+
+      
